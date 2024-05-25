9865545c15ab052f07872e646e1441008025d3e4
ngtnok <105690483+ngtnok@users.noreply.github.com>
2024-05-25 13:15:33 +0900

    add: GET /api/appliances,fetch,option,vite.config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-// import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  // useEffect(async () => {
-  //   const list = await fetch("http:localhost:3000/api/appliances");
-  //   console.log(list);
-  // }, []);
-  return <></>;
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    // fetch("http://localhost:3000/api/appliances")
+    fetch("/api/appliances")
+      .then((res) => res.json())
+      .then((data) => setList(data));
+  });
+  return (
+    <>
+      <ul>
+        {list.map((obj, index) => (
+          <li key={index}>{obj.appliance_name}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
--- src/server.js
+++ src/server.js
@@ -6,6 +6,23 @@ const setupServer = () => {
   app.use("/", express.static(__dirname + "/frontend/public/dist"));
   app.use(express.json());
 
+  const allowCrossDomain = function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, access_token"
+    );
+
+    // intercept OPTIONS method
+    if ("OPTIONS" === req.method) {
+      res.send(200);
+    } else {
+      next();
+    }
+  };
+  app.use(allowCrossDomain);
+
   app.get("/api/appliances", async (req, res) => {
     const appliance = await knex.select().from("appliance");
     res.status(200).send(appliance);
